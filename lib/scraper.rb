49f50d4e4949c262fff6203e45046441ea15e534
edwinlin <edwinlin@gmail.com>
2019-01-15 15:12:27 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,40 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+	# def students
+	# 	@students
+	# end
 
   def self.scrape_index_page(index_url)
-    
+
+    html = File.read(index_url)
+    scrape = Nokogiri::HTML(html)
+
+	result = scrape.css("div.student-card").map do |post|
+		{:name => post.css("h4.student-name").text, :location => post.css("p.student-location").text, :profile_url => post.css("a")[0]["href"]}
+	end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = File.read(profile_url)
+    scrape = Nokogiri::HTML(html)
+
+    student = {}
+    social = ["facebook", "twitter", "linkedin", "github"]
+
+	scrape.css("div.social-icon-container>a").each_with_index do |post, idx|
+		if social.include?(post["href"].sub(/^https?\:\/\/(www.)?/,'').split(".com")[0])
+			symbol = post["href"].sub(/^https?\:\/\/(www.)?/,'').split(".com")[0].to_sym
+		else
+			symbol = "blog".to_sym
+		end
+			
+		student[symbol] = post["href"]
+	end
+		student[:profile_quote] = scrape.css(".vitals-text-container").css(".profile-quote").text
+		student[:bio] = scrape.css("div.details-container").css(".description-holder p").text
+
+		return student
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    # p student_hash
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    student = @@all[0] 
+    attributes_hash.each do |key, value|
+      word = "@#{key.to_s}"
+      student.instance_variable_set(word, value)
+    end
+    student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
